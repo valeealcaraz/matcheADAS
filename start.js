2f596743ec364bde2dbf59bdb8931303eb991c48
Lourdes <lourdeslunaweb@gmail.com>
2021-06-21 20:28:42 -0300

    alert game difficult was added into welcome alert

--- start.js
+++ start.js
@@ -1,32 +1,30 @@
-
-// swal(
-//     "¡Bienvenida!",
-//     "En MatcheADAS tu obetivo es juntar tres o más ítems del mismo tipo, ya sea en fila o columna. Para eso selecciona un ítem, y a continuación un ítem adyacente para intercambiarlos de lugar. Si se forma un grupo, esos ítems se eliminarán y ganarás puntos. ¡Sigue armando grupos de 3 o más antes de que se acabe el tiempo!",
-//     {buttons:"¡A jugar!",
-// });
-
-
-
-
-swal("Nuevo Juego", "Elige el nivel de dificultad", {
-    buttons: {
-        easy: "Fácil",
-        normal: "Normal",
-        hard: "Difícil",
-    },
-})
-    .then((value) => {
-        switch (value) {
-            case "easy":
-                var width = 9;
-                break;
-            case "normal":
-                var width = 8;
-                break;
-            case "hard":
-                var width = 7;
-                break;
-        }
-        createBoard(width)
-    });
+swal(
+    "¡Bienvenida!",
+    "En MatcheADAS tu obetivo es juntar tres o más ítems del mismo tipo, ya sea en fila o columna. Para eso selecciona un ítem, y a continuación un ítem adyacente para intercambiarlos de lugar. Si se forma un grupo, esos ítems se eliminarán y ganarás puntos. ¡Sigue armando grupos de 3 o más antes de que se acabe el tiempo!",
+    {
+        buttons: "¡A jugar!",
+    })
+    .then(() => {
+        swal("Nuevo Juego", "Elige el nivel de dificultad", {
+            buttons: {
+                easy: "Fácil",
+                normal: "Normal",
+                hard: "Difícil",
+            },
+        })
+            .then((value) => {
+                switch (value) {
+                    case "easy":
+                        var width = 9;
+                        break;
+                    case "normal":
+                        var width = 8;
+                        break;
+                    case "hard":
+                        var width = 7;
+                        break;
+                }
+                createBoard(width)
+            });
+    })
 
