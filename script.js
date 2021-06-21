31b54472769048ff12192c85268a3625d1570e79
Lourdes <lourdeslunaweb@gmail.com>
2021-06-21 19:00:13 -0300

    Alert Sweet from de beginning of the game was added

--- script.js
+++ script.js
@@ -5,36 +5,23 @@ const animals = ['🐶','🐱','🐰','🦊','🐷','🐸']
 
 const createBoard = width => {
     let itemWidth = 504/width;
-
     for (i=0 ; i<width*width ; i++) {
         let item = document.createElement("div");
-
         let animalRandom = animals[Math.floor(Math.random() * animals.length)]
-
-
         let inItem = document.createTextNode(animalRandom);
-
-        
         board.appendChild(item);
         item.appendChild(inItem);
-        
         item.style.display = "flex";
         item.style.alignItems = "center";
         item.style.justifyContent = "center";
-        item.style.backgroundColor = "red";
+        item.style.backgroundColor = "rgb(214, 181, 243)";
         item.style.width = `${itemWidth}px`;
         item.style.height = `${itemWidth}px`;
-        
         item.style.fontSize = `${itemWidth/1.5}px`;
-
-        
-
     }
 }
 
-console.log(createBoard(5))
 
 
 
- 
 
