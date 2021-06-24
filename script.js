9964b60e2eb076e323fd0e5204710cc69834ab68
Valeria Alcaraz Areco <vale.alcaraz14@gmail.com>
2021-06-24 09:50:20 -0400

    Id and classes for the board and items

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
 const board = document.getElementById("board");
+const itemsArray = [];
+const animals = ['🐶','🐱','🐰','🦊','🐷','🐸'];
 
 
-const animals = ['🐶','🐱','🐰','🦊','🐷','🐸']
+let contador = 0;
 
 const createBoard = width => {
     let itemWidth = 504/width;
-    for (i=0 ; i<width*width ; i++) {
+    for (let i=0 ; i<width*width ; i++) {
         let item = document.createElement("div");
         let animalRandom = animals[Math.floor(Math.random() * animals.length)]
         let inItem = document.createTextNode(animalRandom);
+        
         board.appendChild(item);
         item.appendChild(inItem);
+        itemsArray.push(item);
+        
+        board.classList.add("board")
         item.style.display = "flex";
         item.style.alignItems = "center";
         item.style.justifyContent = "center";
-        item.style.backgroundColor = "rgb(214, 181, 243)";
+        //item.style.backgroundColor = "rgb(214, 181, 243)";
         item.style.width = `${itemWidth}px`;
         item.style.height = `${itemWidth}px`;
         item.style.fontSize = `${itemWidth/1.5}px`;
+        item.setAttribute("id",i)
+        //item.addEventListener("click",select)   // ESTO LO VEO DESPUÉS
+        
     }
 }
 
-
-
+createBoard(8)
 
 
